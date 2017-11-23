c40350d2784f63f199183c392f481c8241e7a7e1
Thomas Dufour <td@thomasdufour.fr>
2017-11-23 16:17:08 +0100

    feat: make ANSI colours optional, as they break INtelliJ

--- auto-diff-core/src/main/scala/fr/thomasdufour/autodiff/Difference.scala
+++ auto-diff-core/src/main/scala/fr/thomasdufour/autodiff/Difference.scala
@@ -3,18 +3,20 @@ package fr.thomasdufour.autodiff
 import cats.Show
 import cats.data.NonEmptyList
 
+import util.Colouring
+
 sealed trait Difference
 case class ValueDifference( left: String, right: String )                          extends Difference
 case class TaggedDifference( tag: String, difference: Difference )                 extends Difference
 case class ObjectDifference( tpe: String, fields: NonEmptyList[TaggedDifference] ) extends Difference
 
 object Difference {
-  private def show( difference: Difference ): String = difference match {
-    case ValueDifference( l, r )   => s"${util.red( l )} -> ${util.green( r )}"
+  private def show( difference: Difference )( implicit C: Colouring ): String = difference match {
+    case ValueDifference( l, r )   => s"${C.wrapLeft( l )} -> ${C.wrapRight( r )}"
     case TaggedDifference( n, d )  => s"$n = ${show( d )}"
     case ObjectDifference( n, fs ) => fs.toList.map( show( _ ) ).mkString( s"$n(..., ", ", ", ")" )
   }
 
-  implicit val showDifference: Show[Difference] = show
+  implicit def showDifference( implicit C: Colouring ): Show[Difference] = show
 
 }
--- auto-diff-core/src/main/scala/fr/thomasdufour/autodiff/util.scala
+++ auto-diff-core/src/main/scala/fr/thomasdufour/autodiff/util.scala
@@ -22,4 +22,25 @@ object util {
   def red( s: String ): String = Console.RED + s + Console.RESET
 
   def green( s: String ): String = Console.GREEN + s + Console.RESET
+
+  abstract class Colouring {
+    def wrapLeft( s: String ): String
+    def wrapRight( s: String ): String
+  }
+
+  object Colouring {
+    def instance( left: String => String, right: String => String ): Colouring =
+      new Colouring {
+        override def wrapLeft( s: String ): String  = left( s )
+        override def wrapRight( s: String ): String = right( s )
+      }
+
+    implicit val None: Colouring = Colouring.instance( identity, identity )
+
+    val Ansi: Colouring = Colouring.instance( red, green )
+  }
+
+  object AnsiColouring {
+    implicit val Ansi: Colouring = Colouring.Ansi
+  }
 }
