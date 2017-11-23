95da140defa906c819851ef29fb439dc71abe15a
Thomas Dufour <td@thomasdufour.fr>
2017-11-23 11:03:19 +0100

    feat: Diff.diff utility

--- auto-diff-core/src/main/scala/fr/thomasdufour/autodiff/Diff.scala
+++ auto-diff-core/src/main/scala/fr/thomasdufour/autodiff/Diff.scala
@@ -1,9 +1,13 @@
 package fr.thomasdufour.autodiff
 
+import cats.Show
+
 trait Diff[A] {
   def apply( left: A, right: A ): Option[Difference]
 
   def show( value: A ): String
+
+  def diff( left: A, right: A ): Option[String] = apply( left, right ).map( Show[Difference].show )
 }
 
 object Diff extends LowPriorityDiffImplicits {
