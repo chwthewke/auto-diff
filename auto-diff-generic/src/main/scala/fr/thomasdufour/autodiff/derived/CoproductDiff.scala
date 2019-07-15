b04b00d990034a105bd5a990dd1afce55d13e05b
Thomas Dufour <td@thomasdufour.fr>
2019-07-15 13:24:05 +0200

    fix: Coproduct Diff show
    Add an intermediate type class to allow show for coproducts to use
    either the Diff instance for a member (used for show) or a short tag
    consisting of the constructor (used for diffs when the constructors
    differ).

---
+++ auto-diff-generic/src/main/scala/fr/thomasdufour/autodiff/derived/CoproductDiff.scala
@@ -0,0 +1,58 @@
+package fr.thomasdufour.autodiff
+package derived
+
+import cats.syntax.option._
+import shapeless.labelled.FieldType
+import shapeless.:+:
+import shapeless.CNil
+import shapeless.Coproduct
+import shapeless.Inl
+import shapeless.Inr
+import shapeless.OrElse
+import shapeless.Witness
+
+trait CoproductDiff[C <: Coproduct] {
+  def apply( left: C, right: C ): Option[Difference]
+
+  def show( value: C ): String
+
+  def tag( value: C ): String
+}
+
+object CoproductDiff {
+  implicit val cnilTag: CoproductDiff[CNil] =
+    new CoproductDiff[CNil] {
+      // $COVERAGE-OFF$
+      override def apply( left: CNil, right: CNil ): Option[Difference] = none
+
+      override def show( value: CNil ): String = value.impossible
+
+      override def tag( value: CNil ): String = value.impossible
+      // $COVERAGE-ON$
+    }
+
+  implicit def cconsTag[K <: Symbol, V, C <: Coproduct](
+      implicit K: Witness.Aux[K],
+      V: Diff[V] OrElse MkDiff[V],
+      T: CoproductDiff[C]
+  ): CoproductDiff[FieldType[K, V] :+: C] =
+    new CoproductDiff[FieldType[K, V] :+: C] {
+
+      private def showTag( value: FieldType[K, V] :+: C ): String = s"${tag( value )}(...)"
+
+      override def apply( left: FieldType[K, V] :+: C, right: FieldType[K, V] :+: C ): Option[Difference] =
+        ( left, right ) match {
+          case ( Inl( l ), Inl( r ) ) => V.unify.apply( l, r )
+          case ( Inr( l ), Inr( r ) ) => T( l, r )
+          case _                      => Difference.Value( showTag( left ), showTag( right ) ).some
+        }
+
+      override def show( value: FieldType[K, V] :+: C ): String =
+        value.eliminate( V.unify.show, T.show )
+
+      override def tag( value: FieldType[K, V] :+: C ): String = value match {
+        case Inl( _ ) => K.value.name
+        case Inr( x ) => T.tag( x )
+      }
+    }
+}
--- auto-diff-generic/src/main/scala/fr/thomasdufour/autodiff/derived/MkDiff.scala
+++ auto-diff-generic/src/main/scala/fr/thomasdufour/autodiff/derived/MkDiff.scala
@@ -2,20 +2,12 @@ package fr.thomasdufour.autodiff
 package derived
 
 import cats.data.NonEmptyList
-import cats.syntax.option._
 import scala.annotation.implicitNotFound
 import scala.reflect.ClassTag
-import shapeless.labelled.FieldType
-import shapeless.:+:
-import shapeless.CNil
 import shapeless.Coproduct
 import shapeless.HList
-import shapeless.Inl
-import shapeless.Inr
 import shapeless.LabelledGeneric
 import shapeless.Lazy
-import shapeless.OrElse
-import shapeless.Witness
 
 @implicitNotFound( "Could not derive an instance of Diff[${A}]" )
 trait MkDiff[A] extends Diff[A]
@@ -25,35 +17,10 @@ object MkDiff extends MkDiffDerivation {
 }
 
 private[derived] abstract class MkDiffDerivation {
-  implicit val mkDiffCnil: MkDiff[CNil] =
-    new MkDiff[CNil] {
-      // $COVERAGE-OFF$
-      override def apply( left: CNil, right: CNil ): Option[Difference] = none
-
-      override def show( value: CNil ): String = value.impossible
-      // $COVERAGE-ON$
-    }
-
-  implicit def mkDiffCcons[K <: Symbol, V, T <: Coproduct](
-      implicit K: Witness.Aux[K],
-      V: Diff[V] OrElse MkDiff[V],
-      T: MkDiff[T]
-  ): MkDiff[FieldType[K, V] :+: T] =
-    new MkDiff[FieldType[K, V] :+: T] {
-      override def apply( left: FieldType[K, V] :+: T, right: FieldType[K, V] :+: T ): Option[Difference] =
-        ( left, right ) match {
-          case ( Inl( l ), Inl( r ) ) => V.unify.apply( l, r )
-          case ( Inr( l ), Inr( r ) ) => T( l, r )
-          case _                      => Difference.Value( show( left ), show( right ) ).some
-        }
-
-      override def show( value: FieldType[K, V] :+: T ): String =
-        value.eliminate( _ => s"${K.value.name}(...)", T.show )
-    }
 
   implicit def mkDiffGenericCoproduct[T, R <: Coproduct](
       implicit gen: LabelledGeneric.Aux[T, R],
-      diffR: Lazy[MkDiff[R]],
+      diffR: Lazy[CoproductDiff[R]],
       tag: ClassTag[T]
   ): MkDiff[T] =
     new MkDiff[T] {
