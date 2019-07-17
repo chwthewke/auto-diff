1579255a0856fe41c74c693bd5c8d89510fe2ae0
Thomas Dufour <td@thomasdufour.fr>
2019-07-17 13:27:56 +0200

    chore: travis script uploads coverage for 2.13 build only

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -5,4 +5,4 @@ sbt_cmd="sbt ++$TRAVIS_SCALA_VERSION"
 $sbt_cmd package && \
   $sbt_cmd coverage test coverageReport && \
   $sbt_cmd publishLocal && \
-  bash <(curl -s https://codecov.io/bash)
+  ( if [[ "$TRAVIS_SCALA_VERSION" == "2.13.0" ]]; then bash <(curl -s https://codecov.io/bash); fi )
