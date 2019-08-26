4ad8faec2df0586ed25fc91d3825f95bb63397d3
Thomas Dufour <td@thomasdufour.fr>
2019-08-26 22:26:00 +0200

    WIP update cats to 2.0.0-RC2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,19 +6,22 @@ object Dependencies {
   type D = Seq[ModuleID]
 
   val kindProjector: D = Seq(
-    compilerPlugin( "org.typelevel" %% "kind-projector" % "0.10.3" cross CrossVersion.binary ) )
+    compilerPlugin( "org.typelevel" %% "kind-projector" % "0.10.3" cross CrossVersion.binary )
+  )
 
   val splain: D = Seq( compilerPlugin( "io.tryp" % "splain" % "0.4.1" cross CrossVersion.patch ) )
 
-  val catsVersion = "2.0.0-M4"
+  val catsVersion = "2.0.0-RC2"
 
   val cats: D = Seq( "org.typelevel" %% "cats-core" % catsVersion )
 
   val shapeless: D = Seq( "com.chuusai" %% "shapeless" % "2.3.3" )
 
   val scalacheck: D =
-    Seq( "org.scalacheck"   %% "scalacheck"      % "1.14.0"   % Test,
-        "io.chrisdavenport" %% "cats-scalacheck" % "0.2.0-M1" % Test )
+    Seq(
+      "org.scalacheck"    %% "scalacheck"      % "1.14.0"   % Test,
+      "io.chrisdavenport" %% "cats-scalacheck" % "0.2.0-M1" % Test exclude ("org.typelevel", "cats-core")
+    )
 
   val scalatest: D = Seq( "org.scalatest" %% "scalatest" % "3.0.8" )
 
@@ -30,7 +33,7 @@ object Dependencies {
     "org.scala-lang" % "scala-library"  % scalaVersion.value,
     "org.scala-lang" % "scala-compiler" % scalaVersion.value,
     "org.scala-lang" % "scala-reflect"  % scalaVersion.value,
-    "org.typelevel"  %% "cats-core"     % catsVersion,
+    "org.typelevel"  %% "cats-core"     % catsVersion
   ) ++ scalacheck
 
   val common: D = kindProjector ++ cats
